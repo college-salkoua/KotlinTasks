52e32fb4cb2ca30b76e43db90d4ba3da7fc9b53c
salko-ua <chips69jamil69game@gmail.com>
2024-03-15 17:48:26 +0200

    add PW1

---
+++ src/PW1/main.kts
@@ -0,0 +1,39 @@
+fun main() {
+    println("Cloudy"); println("Partly Cloudy"); println("Windy")
+}
+
+fun main() {
+    var counter = 17
+    println("Сьогодні на парі були присутні $counter студентів);
+}
+
+fun main() {
+    var counter = 17
+    println("Сьогодні на парі були присутні ${counter +1} студентів");
+}
+
+fun main() {
+    var name = "Marry"
+    var age = 20
+    println("$name is $age year old")
+}
+
+fun main() {
+    var a = 100
+    var b = "log message"
+    var c = 3.14
+    var d = 100_000_000_000_000
+    var e = false
+    println(a); println(b); println(c); println(d); println(e)
+}
+
+fun main() {
+    val a = "Добрий "
+    val b = "День "
+    println("$a $b")
+    println("$b $a")
+}
+
+
+
+
